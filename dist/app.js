7fd81abdf7b60df2e69a22087e1ca526b7c7a05f
Rahul Sutradhar <rahulsd380@gmail.com>
2024-09-30 20:40:40 +0600

    CORS issue solved

--- dist/app.js
+++ dist/app.js
@@ -15,7 +15,7 @@ app.use((0, cookie_parser_1.default)());
 // Middleware for parsing JSON bodies
 app.use(express_1.default.json());
 // Middleware for handling CORS with credentials
-app.use((0, cors_1.default)({ origin: ['http://localhost:5173', 'https://orbit-rides.vercel.app/'], credentials: true }));
+app.use((0, cors_1.default)({ origin: ['https://orbit-rides.vercel.app'], credentials: true }));
 // Root route
 app.get('/', (req, res) => {
     res.send("Welcome to bike rental");
